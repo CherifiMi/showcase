4f21c5a880c2a2c992605d094735f415eac3964e
CherifiMi <cherifi154949@gmail.com>
2023-06-15 03:42:07 +0100

    .

---
+++ app/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.symbolProcessing)
+    alias(libs.plugins.safeArgs)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.testLogger)
+    alias(libs.plugins.junit5Android)
+}
+
+android {
+    namespace = "com.example.showcase"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.showcase"
+        minSdk = 26
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles("proguard-android.txt", "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion  = libs.versions.kotlinCompilerExtensionVersion.get()
+    }
+    /*packagingOptions {
+        resources {
+            excludes += '/META-INF/{AL2.0,LGPL2.1}'
+        }
+    }*/
+}
+
+dependencies {
+    implementation(projects.featureAlbum)
+    implementation(projects.featureProfile)
+    implementation(projects.featureFavourite)
+}
---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+//@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.safeArgs) apply false
+    alias(libs.plugins.junit5Android) apply false
+    alias(libs.plugins.spotless)
+    alias(libs.plugins.detekt)
+}
+
+/*
+configure(){
+    configure{
+        parallel = true
+        ignoreFailures = false
+        setSource(file(projectDir))
+    }
+}*/
