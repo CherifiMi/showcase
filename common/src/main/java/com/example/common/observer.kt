47efcab114060df0c6eea0843998b43ff1845298
CherifiMi <cherifi154949@gmail.com>
2023-06-15 11:19:24 +0100

    .

--- common/src/main/java/com/example/common/observer.kt
+++ common/src/main/java/com/example/common/observer.kt
@@ -4,11 +4,11 @@ import kotlin.properties.ObservableProperty
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
-inline fun<T> observer(
-    initValue: T,
-    crossinline onChange: (newValue: T)-> Unit
+inline fun <T> observer(
+    initialValue: T,
+    crossinline onChange: (newValue: T) -> Unit,
 ): ReadWriteProperty<Any?, T> =
-    object : ObservableProperty<T>(initValue) {
+    object : ObservableProperty<T>(initialValue) {
         override fun afterChange(property: KProperty<*>, oldValue: T, newValue: T) =
             onChange(newValue)
     }
