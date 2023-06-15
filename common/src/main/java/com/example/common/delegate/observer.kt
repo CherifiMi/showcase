5224a5ae4df3667697a34e77c26547718e079b4d
CherifiMi <cherifi154949@gmail.com>
2023-06-15 11:15:14 +0100

    .

--- common/src/main/java/com/example/common/delegate/observer.kt
+++ common/src/main/java/com/example/common/delegate/observer.kt
@@ -1,4 +1,14 @@
 package com.example.common.delegate
 
-class observer {
-}
+import kotlin.properties.ObservableProperty
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+inline fun<T> observer(
+    initValue: T,
+    crossinline onChange: (newValue: T)-> Unit
+): ReadWriteProperty<Any?, T> =
+    object : ObservableProperty<T>(initValue) {
+        override fun afterChange(property: KProperty<*>, oldValue: T, newValue: T) =
+            onChange(newValue)
+    }
